e0c07687277a7d73a5cdde7bb1c9420e5e188259
SliitMtitProject <logithiya@gmail.com>
2017-06-06 11:11:08 +0530

    Xml: interface

---
+++ app/src/main/java/com/example/home/mynewapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.home.mynewapplication;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
